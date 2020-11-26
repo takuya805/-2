aec42e778eecc707987a0c4dbeec5fae71f566dc
takuya805 <takuya0903exile@gmail.com>
2020-11-26 09:50:01 +0000

    [課題2提出]

---
+++ development1_app/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'books#top'
+  resources :books
+  delete '/books/:id' => 'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
